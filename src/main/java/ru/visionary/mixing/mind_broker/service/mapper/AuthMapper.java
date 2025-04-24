f02b661876ff177df39ae5b8ba73e5d5146402f4
Khanin Dmitriy <dmitrii.khanin@gmail.com>
2025-04-24 21:13:26 +0300

    SCRUM-122 Пароль не шифруется

--- src/main/java/ru/visionary/mixing/mind_broker/service/mapper/AuthMapper.java
+++ src/main/java/ru/visionary/mixing/mind_broker/service/mapper/AuthMapper.java
@@ -1,10 +1,27 @@
 package ru.visionary.mixing.mind_broker.service.mapper;
 
 import org.mapstruct.Mapper;
-import ru.visionary.mixing.mind_broker.entity.User;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import ru.visionary.mixing.generated.model.RegisterRequest;
+import ru.visionary.mixing.mind_broker.entity.User;
 
 @Mapper(componentModel = "spring")
-public interface AuthMapper {
-    User requestToUser(RegisterRequest request);
+public abstract class AuthMapper {
+    private PasswordEncoder passwordEncoder;
+
+    @Mapping(target = "password", qualifiedByName = "password")
+    public abstract User requestToUser(RegisterRequest request);
+
+    @Named("password")
+    protected String convertPassword(String password) {
+        return passwordEncoder.encode(password);
+    }
+
+    @Autowired
+    public void setPasswordEncoder(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
 }
