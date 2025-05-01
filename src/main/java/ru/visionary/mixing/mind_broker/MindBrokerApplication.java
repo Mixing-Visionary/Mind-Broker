0800d4f5980dfbf800afac85f060a27e6c96c250
Khanin Dmitriy <dmitrii.khanin@gmail.com>
2025-05-02 01:40:43 +0300

    release v0.0.7

--- src/main/java/ru/visionary/mixing/mind_broker/MindBrokerApplication.java
+++ src/main/java/ru/visionary/mixing/mind_broker/MindBrokerApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.Arrays;
+
 @SpringBootApplication
 @EnableConfigurationProperties
 @ConfigurationPropertiesScan
@@ -14,6 +16,10 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 public class MindBrokerApplication {
     public static void main(String[] args) {
-        SpringApplication.run(MindBrokerApplication.class, args);
+        if (Arrays.asList(args).contains("migrations")) {
+            SpringApplication.run(MindBrokerApplication.class, args).close();
+        } else {
+            SpringApplication.run(MindBrokerApplication.class, args);
+        }
     }
 }
